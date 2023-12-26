c85212ad3b50034302a706c7bec5983fff7b2f29
continuum1 <jackamedders1106@gmail.com>
2023-12-25 22:11:39 -0500

    Various edits & implemented rectangle overlap alg.
    I worked it out in a physical notebook (thats new to me) and decided the
    best solution is a corner detection case-by-case solver. There are many
    convenient restrictions that let this be realistically solvable by mioa:
    the rectangles are on a grid, both aligned on the same axes and discrete
    in measure (single pixel precision).

--- index.js
+++ index.js
@@ -3,21 +3,32 @@ function arrange(slates, width, height) {
 }
 
 function overlap(pos1, pos2, dim1, dim2) {
+    if(pos1[0] <= pos2[0] && 
+       pos1[1] <= pos2[1] &&
+       pos1[0] + dim1[0] >= pos2[0] + dim2[0] &&
+       pos1[1] + dim1[1] >= pos2[1] + dim2[1]) return dim2[0] * dim2[1]; //case 9
+
     c1 = hasPoint(pos1, pos2, dim2);
     c2 = hasPoint([pos1[0] + dim1[0], pos1[1]], pos2, dim2);
     c3 = hasPoint([pos1[0], pos1[1] + dim1[1]], pos2, dim2);
     c4 = hasPoint([pos1[0] + dim1[0], pos1[1] + dim1[1]], pos2, dim2);
 
-    if(c1 && c2 && c3 && c4) return (dim1[0] * dim1[1]);
-//    if()
+    if(c1 && c2 && c3 && c4) return (dim1[0] * dim1[1]); //case 0
+    if(!c1 && !c2 && c3 && !c4) return (pos1[0] + dim1[0] - pos2[0]) * (pos1[1] + dim1[1] - pos2[1]); //case 1
+    if(!c1 && !c2 && !c3 && c4) return (pos2[0] + dim2[0] - pos1[0]) * (pos1[1] + dim1[1] - pos2[1]); //case 2
+    if(!c1 && c2 && !c3 && !c4) return (pos1[0] + dim1[0] - pos2[0]) * (pos2[1] + dim2[1] - pos1[1]); //case 3
+    if(c1 && !c2 && !c3 && !c4) return (pos2[0] + dim2[0] - pos1[0]) * (pos2[1] + dim2[1] - pos1[1]); //case 4
+    if(c1 && c2 && !c3 && !c4) return dim1[0] * (pos2[1] + dim2[1] - pos1[1]); //case 5
+    if(!c1 && c2 && c3 && !c4) return (pos1[0] + dim1[0] - pos2[0]) * dim1[1]; //case 6
+    if(!c1 && !c2 && c3 && c4) return dim1[0] * (pos1[1] + dim1[1] - pos2[1]); //case 7
+    if(c1 && !c2 && !c3 && c4) return (pos2[0] + dim2[0] - pos1[0]) * dim1[1]; //case 8
 
-
-    return 0;
+    return 0; //case 10
 }
 
 function hasPoint(pos1, pos2, dim2) {
-    return (pos1[0] > pos2[0] && 
-            pos1[1] > pos2[1] && 
-            pos1[0] < (pos2[0] + dim2[0]) && 
-            pos1[1] < (pos2[1] + dim2[1]));
+    return (pos1[0] >= pos2[0] && 
+            pos1[1] >= pos2[1] && 
+            pos1[0] <= (pos2[0] + dim2[0]) && 
+            pos1[1] <= (pos2[1] + dim2[1]));
 }
